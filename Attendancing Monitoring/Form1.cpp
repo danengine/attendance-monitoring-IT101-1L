b338ae37c9fdae5b89fa41a5b74bd1783837740d
DanEngine <daniloeslawanmusic@gmail.com>
2025-02-25 16:34:29 +0800

    New UI
    Added  new UI
    Added Auto Fill Information

--- Attendancing Monitoring/Form1.cpp
+++ Attendancing Monitoring/Form1.cpp
@@ -1,3 +1,6 @@
 #include "pch.h"
+#using <System.dll>
+#using <System.Net.Http.dll>
+#using <System.IO.dll>
 // #include "Form1.h"
 
--- Attendancing Monitoring/Form1.h
+++ Attendancing Monitoring/Form1.h
@@ -35,6 +35,23 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: bool dragging = false;
+	private: System::Drawing::Point offset;
+	private: System::Windows::Forms::Button^ button2;
+
 	protected:
 
 	protected:
@@ -57,23 +74,177 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(63, 66);
+			this->button1->Location = System::Drawing::Point(753, 119);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(192, 25);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Attendance";
+			this->button1->Text = L"Auto Fill";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->listView1);
+			this->panel1->Location = System::Drawing::Point(12, 106);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(585, 393);
+			this->panel1->TabIndex = 1;
+			// 
+			// listView1
+			// 
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(3, 3);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(579, 481);
+			this->listView1->TabIndex = 0;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(611, 122);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(136, 20);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(608, 106);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(58, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Student ID";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(611, 174);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(334, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(611, 224);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(608, 158);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Name";
+			this->label2->Click += gcnew System::EventHandler(this, &Form1::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(608, 208);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(46, 13);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Program";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(826, 208);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(31, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"email";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(718, 208);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(29, 13);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Year";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(720, 224);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 10;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(829, 224);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 20);
+			this->textBox5->TabIndex = 11;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->pictureBox1->ImageLocation = L"MCM.png";
+			this->pictureBox1->Location = System::Drawing::Point(12, 9);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(278, 91);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 12;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &Form1::pictureBox1_Click);
+			this->pictureBox1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &Form1::pictureBox1_MouseDown);
+			this->pictureBox1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &Form1::pictureBox1_MouseMove);
+			this->pictureBox1->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &Form1::pictureBox1_MouseUp);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::IndianRed;
+			this->button2->Location = System::Drawing::Point(916, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(37, 34);
+			this->button2->TabIndex = 13;
+			this->button2->Text = L"X";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(976, 554);
+			this->ClientSize = System::Drawing::Size(957, 554);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->button1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->MaximizeBox = false;
@@ -83,17 +254,93 @@ namespace CppCLRWinFormsProject {
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Attendance Monitoring";
 			this->TopMost = true;
-			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		String^ s = "Bleeeeee";
-		MessageBox::Show(s);	
+		try {
+			String^ studentId = textBox1->Text;
+
+			String^ url = "https://api.danengine.tech/v1/student/project?studentId=" + studentId;
+
+			System::Net::HttpWebRequest^ request = (System::Net::HttpWebRequest^)System::Net::WebRequest::Create(url);
+			request->Method = "GET";
+
+			System::Net::HttpWebResponse^ response = (System::Net::HttpWebResponse^)request->GetResponse();
+			System::IO::StreamReader^ reader = gcnew System::IO::StreamReader(response->GetResponseStream());
+			String^ jsonResponse = reader->ReadToEnd();
+
+			reader->Close();
+			response->Close();
+
+			String^ studentIdStr = ExtractValue(jsonResponse, "\"id\":\"", "\"");
+			String^ studentName = ExtractValue(jsonResponse, "\"name\":\"", "\"");
+			String^ studentLevel = ExtractValue(jsonResponse, "\"level\":\"", "\"");
+			String^ studentProgram = ExtractValue(jsonResponse, "\"program\":\"", "\"");
+			String^ studentEmail = ExtractValue(jsonResponse, "\"email\":\"", "\"");
+
+			textBox2->Text = studentName;
+			textBox3->Text = studentProgram;
+			textBox4->Text = studentLevel;
+			textBox5->Text = studentEmail;
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show("Student Not Found", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		}
 	}
-	};
+
+		   String^ ExtractValue(String^ source, String^ startDelimiter, String^ endDelimiter) {
+			   // Remove all unnecessary spaces to prevent mismatches
+			   source = source->Replace(" ", "")->Replace("\n", "")->Replace("\r", "");
+
+			   int startIndex = source->IndexOf(startDelimiter);
+			   if (startIndex == -1) {
+				   MessageBox::Show("Delimiter not found: " + startDelimiter, "Debug", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+				   return "N/A"; // Not found
+			   }
+
+			   startIndex += startDelimiter->Length;
+			   int endIndex = source->IndexOf(endDelimiter, startIndex);
+
+			   if (endIndex == -1) {
+				   MessageBox::Show("End delimiter not found: " + endDelimiter, "Debug", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+				   return "N/A"; // Not found
+			   }
+
+			   return source->Substring(startIndex, endIndex - startIndex);
+		   }
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		dragging = true;
+		offset = System::Drawing::Point(e->X, e->Y); // Get the mouse position relative to the PictureBox
+	}
+
+	private: System::Void pictureBox1_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		if (dragging) {
+			// Calculate new form position
+			System::Drawing::Point newLocation = this->PointToScreen(System::Drawing::Point(e->X, e->Y));
+			newLocation.X -= offset.X;
+			newLocation.Y -= offset.Y;
+			this->Location = newLocation; // Set new form position
+		}
+	}
+	private: System::Void pictureBox1_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		dragging = false;
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		Environment::Exit(0);
+	}
+private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
